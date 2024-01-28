55bdd8495873f21bfd3353e7a40c2c72f51d8717
Makayla Hu <makkhu18@gmail.com>
2024-01-28 14:11:28 -0800

    structure edits

--- rubiks-cube/src/Cube.java
+++ rubiks-cube/src/Cube.java
@@ -1,8 +1,21 @@
 public class Cube {
 
-    private Face[] faces;
+    private Face front;
+    private int size;
 
     public Cube() {
-
+        size = 3;
     }
+
+    public int getSize() {
+        return size;
+    }
+
+    /*
+    * MOVES
+    * - follows pattern of faceDirection
+    * - C is clockwise; CC is counterclockwise
+    * */
+
+
 }
--- rubiks-cube/src/Face.java
+++ rubiks-cube/src/Face.java
@@ -1,6 +1,27 @@
 public class Face {
 
-    String[][] colorGrid;
+    private String[][] colorGrid;
+    private Cube homeCube;
+    private Face right;
+    private Face left;
+    private Face up;
+    private Face down;
 
+    public Face(Cube cube, String color) {
+        homeCube = cube;
 
+        for (int row = 0; row < homeCube.getSize(); row++) {
+            for (int col = 0; col < homeCube.getSize(); col++) {
+                colorGrid[row][col] = color;
+            }
+        }
+    }
+
+    /*
+    * MOVES
+    * - turns face clockwise or counterclockwise
+    * */
+    public void clockwise() {}
+
+    public void counterclockwise() {}
 }
