01df2a94c62d49ed417fab0117365826839647ef
Makayla Hu <makkhu18@gmail.com>
2024-02-01 00:09:40 -0800

    updated addFace method

--- rubiks-cube/src/Cube.java
+++ rubiks-cube/src/Cube.java
@@ -15,24 +15,42 @@ public class Cube {
      * can only add in (1) UP and (2) RIGHT directions
      * */
     public void addFace(Face f1, Face f2, String direction) {
-        //treat f1 as main face, adding f2 above f1
-        if (direction == "UP") {
-            f1.getUp().setDown(f2);
-            f1.setUp(f2);
-            f2.getDown().setUp(f1);
-            f2.setDown(f1);
-            f2.setRight(f1.getRight());
-            f2.setLeft(f1.getLeft());
+        int numRotations = 0;
+        try {
+            if (direction == "UP") {
+                numRotations = 0;
+            } else if (direction == "RIGHT") {
+                numRotations = 1;
+            } else if (direction == "DOWN") {
+                numRotations = 2;
+            } else if (direction == "LEFT") {
+                numRotations = 3;
+            }
+        } catch (Exception e) {
+            System.out.println("Can only add Face in UP, RIGHT, DOWN, or LEFT direction");
         }
-        //treating f1 as main face, adding f2 to the right of f1
-        else {
-            f1.getRight().setLeft(f2);
-            f1.setRight(f2);
-            f2.getLeft().setRight(f1);
-            f2.setLeft(f1);
-            f2.setUp(f1.getUp());
-            f2.setDown(f1.getDown());
+        for (int rep = 0; rep < numRotations; rep++) {
+            rotateCube("counterclockwise");
         }
+
+        f1.getUp().setDown(f2);
+        f1.setUp(f2);
+        f2.getDown().setUp(f1);
+        f2.setDown(f1);
+
+        f2.getRight().setLeft(f1.getRight());
+        f2.setRight(f1.getRight());
+        f1.getRight().getUp().setDown(f2);
+        f1.getRight().setUp(f2);
+
+        f2.getLeft().setRight(f1.getLeft());
+        f2.setLeft(f1.getLeft());
+        f1.getLeft().getUp().setDown(f2);
+        f1.getLeft().setUp(f2);
+    }
+
+    public void rotateCube(String direction) {
+
     }
 
     public int getSize() {return this.size;}
--- rubiks-cube/src/ThreeByThreeCube.java
+++ rubiks-cube/src/ThreeByThreeCube.java
@@ -2,8 +2,5 @@ public class ThreeByThreeCube extends Cube {
 
     private static final int size = 3;
 
-    @Override
-    public int getSize() {return size;}
-
     public void rotate(String direction) {}
 }
