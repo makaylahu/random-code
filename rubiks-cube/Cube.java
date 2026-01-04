5af1a998db1ce5ea4b9e88283c647d6178a16c21
Makayla Hu <makkhu18@gmail.com>
2026-01-05 06:39:41 +0800

    flushed out colors for Face

--- rubiks-cube/Cube.java
+++ rubiks-cube/Cube.java
@@ -3,5 +3,10 @@ public class Cube {
 
     public Cube () {
         faces = new Face[6];
+        int index = 0;
+
+        for (Colors c : Colors.values()) {
+            faces[index] = new Face(c);
+        }
     }
 }
--- rubiks-cube/Face.java
+++ rubiks-cube/Face.java
@@ -1,6 +1,8 @@
 public class Face {
+    private Colors color;
+    private Colors[] neighbours; //clockwise permutation
     private EdgePiece[] edgePieces;
-    private CornerPiece[] cornerPieces;
+    private CornerPiece[] cornerPieces; //clockwise permutation
 
     private class EdgePiece {
         private Colors[] colors;
@@ -19,14 +21,38 @@ public class Face {
     }
 
     public Face (Colors c) {
+        this.color = c;
+        switch (c) {
+            case WHITE:
+                neighbours = new Colors[]{Colors.RED, Colors.GREEN, Colors.ORANGE, Colors.BLUE};
+                break;
+            case RED:
+                neighbours = new Colors[]{Colors.WHITE, Colors.BLUE, Colors.YELLOW, Colors.GREEN};
+                break;
+            case BLUE:
+                neighbours = new Colors[]{Colors.WHITE, Colors.ORANGE, Colors.YELLOW, Colors.RED};
+                break;
+            case YELLOW:
+                neighbours = new Colors[]{Colors.ORANGE, Colors.GREEN, Colors.RED, Colors.BLUE};
+                break;
+            case ORANGE:
+                neighbours = new Colors[]{Colors.YELLOW, Colors.BLUE, Colors.WHITE, Colors.GREEN};
+                break;
+            case GREEN:
+                neighbours = new Colors[]{Colors.YELLOW, Colors.ORANGE, Colors.WHITE, Colors.RED};
+                break;
+        }
+
 
     }
 
     public void turn(Directions d) {
         switch (d) {
             case CLOCKWISE:
+                // add mechanism
                 break;
             case COUNTERCLOCKWISE:
+                // add mechanism
                 break;
         }
     }
